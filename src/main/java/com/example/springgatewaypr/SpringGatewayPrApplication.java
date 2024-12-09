7527ed9b059e1a74769d729d12bfb703a6898aa8
saul <dlathf3210@naver.com>
2024-12-09 22:04:36 +0900

    first commit

---
+++ src/main/java/com/example/springgatewaypr/SpringGatewayPrApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgatewaypr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGatewayPrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGatewayPrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springgatewaypr/SpringGatewayPrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springgatewaypr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGatewayPrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
